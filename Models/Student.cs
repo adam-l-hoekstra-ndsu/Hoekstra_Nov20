ae4b70de650041cf5fb3467d15e02b3d3d2edcfc
adam-l-hoekstra-ndsu <adam.l.hoekstra@ndsu.edu>
2024-11-20 14:44:14 -0600

    New Class - Student.cs

---
+++ Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace Hoekstra_Nov20.Models
+{
+    public class Student
+    {
+        public int id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastDescription { get; set; }
+    }
+}
